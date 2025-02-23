c7052063a700b8b8409c7bdf5100e5738d51855f
jordaneaster <jordaneaster222@icloud.com>
2025-02-23 08:42:12 -0500

    fix images

--- next.config.js
+++ next.config.js
@@ -1,8 +1,25 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+
 const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    turbo: false,
+  },
   images: {
     domains: ['api-trestle.corelogic.com'], // Allow external image domain
   },
+  webpack: (config) => {
+    const __dirname = path.dirname(new URL(import.meta.url).pathname);
+    config.resolve.modules.push(path.join(__dirname, 'node_modules'));
+    return config;
+  },
+  env: {
+    TRESTLE_TOKEN_URL: process.env.TRESTLE_TOKEN_URL,
+    TRESTLE_CLIENT_ID: process.env.TRESTLE_CLIENT_ID,
+    TRESTLE_CLIENT_SECRET: process.env.TRESTLE_CLIENT_SECRET,
+  },
 };
+
 
 export default nextConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,24 @@
-/** @type {import('next').NextConfig} */
-import path from 'path';
+// /** @type {import('next').NextConfig} */
+// import path from 'path';
 
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    turbo: false,
-  },
-  images: {
-    domains: ['api-trestle.corelogic.com'], // Allow external image domain
-  },
-  webpack: (config) => {
-    const __dirname = path.dirname(new URL(import.meta.url).pathname);
-    config.resolve.modules.push(path.join(__dirname, 'node_modules'));
-    return config;
-  },
-  env: {
-    TRESTLE_TOKEN_URL: process.env.TRESTLE_TOKEN_URL,
-    TRESTLE_CLIENT_ID: process.env.TRESTLE_CLIENT_ID,
-    TRESTLE_CLIENT_SECRET: process.env.TRESTLE_CLIENT_SECRET,
-  },
-};
+// const nextConfig = {
+//   reactStrictMode: true,
+//   experimental: {
+//     turbo: false,
+//   },
+//   images: {
+//     domains: ['api-trestle.corelogic.com'], // Allow external image domain
+//   },
+//   webpack: (config) => {
+//     const __dirname = path.dirname(new URL(import.meta.url).pathname);
+//     config.resolve.modules.push(path.join(__dirname, 'node_modules'));
+//     return config;
+//   },
+//   env: {
+//     TRESTLE_TOKEN_URL: process.env.TRESTLE_TOKEN_URL,
+//     TRESTLE_CLIENT_ID: process.env.TRESTLE_CLIENT_ID,
+//     TRESTLE_CLIENT_SECRET: process.env.TRESTLE_CLIENT_SECRET,
+//   },
+// };
 
-export default nextConfig;
+// export default nextConfig;
