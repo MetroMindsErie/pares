113995045230f48076bcd01b8916a446d1c3d0f7
jordaneaster <jordaneaster222@icloud.com>
2025-03-05 13:01:30 -0500

    change env var

--- src/pages/api/reels/fetch.js
+++ src/pages/api/reels/fetch.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   // Initialize Supabase client with the request context
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY,
     {
       global: { 
         headers: { cookie: req.headers.cookie } 
